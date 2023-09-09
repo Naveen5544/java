e83f382adb1eff52e9f8e630694c530f5dfbacb1
Naveen5544 <130354649+Naveen5544@users.noreply.github.com>
2023-09-09 09:54:33 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+public class PrimeNumber{
+    public static void main(String args[]){
+        System.out.println("Enter an integer value: ");
+        Scanner sc = new Scanner(System.in);
+        int number=sc.nextInt();
+        int i=2,count=0;
+        while(i<=number/2)
+        {
+            if(number%i == 0){
+                count++;
+                break;
+            }
+            i++;
+        }
+        if(count==0)
+        {
+            System.out.println(number+ " is prime number");
+        }else{
+            System.out.println(number+ " is not a prime number");
+        }
+    }
+}
