c9ca21366ab94dbbf540b36871a51e9c950bd0e1
Naveen5544 <130354649+Naveen5544@users.noreply.github.com>
2023-09-25 11:52:09 +0530

    Add files via upload

---
+++ ThisDemo2.java
@@ -0,0 +1,32 @@
+//To demonstrate the importance of using this keyword
+//09/09/23
+//ThisDemo2.java
+import java.io.*;
+import java.util.*;
+class Student {
+    int rno;
+    String name;
+    double fee;
+    
+   void StudentConstructor(int rno,String name,double fee){
+    rno=rno;
+    name=name;
+    fee=fee;
+    }
+    void display()
+    {
+        System.out.println("The student details are: ");
+        System.out.println(rno+" "+name+" "+fee);
+    }
+}
+class ThisDemo1
+{
+    public static void main(String args[]){
+        Student s1=new Student();
+        Student s2=new Student();
+        s1.StudentConstructor(505,"Naveen",22.500);
+        s2.StudentConstructor(11,"RMVN",99.999);
+        s1.display();
+        s2.display();
+    }
+}
