4d3aa6297038eff5cf0e2766902d827cd42f8332
Naveen5544 <130354649+Naveen5544@users.noreply.github.com>
2023-09-30 12:25:16 +0530

    Add files via upload

---
+++ DefaultDemo.java
@@ -0,0 +1,14 @@
+//29/08/23
+//DefaultDemo.java
+class Sample {
+    void display() {
+        System.out.println("Hello world");
+    }
+}
+
+public class DefaultDemo {
+    public static void main(String[] args) {
+        Sample s = new Sample();
+        s.display();
+    }
+}
